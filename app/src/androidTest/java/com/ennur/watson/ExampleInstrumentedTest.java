bec7b3485d02da3b28dff5079d5b0718b062b03d
Nur Rohman <ennurr31@gmail.com>
2016-11-12 18:09:10 +0700

    3rd commit

--- app/src/androidTest/java/com/ennur/watson/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ennur/watson/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.gooidee.bluemix", appContext.getPackageName());
+        assertEquals("com.ennur.watson", appContext.getPackageName());
     }
 }
